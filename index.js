2cdbf5c17baafe767a7e46cf6b33b554f177c6ef
tsurdilo <tsurdilo@redhat.com>
2019-01-25 23:53:00 -0500

    refactor so can be used as lib and not just cli

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const minimist = require('minimist');
-const infoprompt = require('./lib/infoprompt')
 const usage = require('./lib/usage');
-const appinstall = require('./lib/appinstall');
+const gen = require('./lib/getandinstall');
 
 const allCommands = ['gen'];
-const defaultAppDetails = {
-    capabilities: 'bpm',
-    packagename: 'com.company',
-    name: 'business-application',
-    version: '',
-    options: ['kjar', 'model', 'service']
-};
 
 var site = 'https://start.jbpm.org/gen';
 var dounzip = false;
@@ -31,47 +23,6 @@ console.log(
         })
     )
 );
-
-const genAndInstall = async (args) => {
-    var appDetails = {};
-    if (quickinstall) {
-        appDetails = defaultAppDetails;
-        // if quickstart check of individual params
-        // and overwrite defaults
-        if (args.capabilities) {
-            appDetails.capabilities = args.capabilities;
-        }
-        if (args.packagename) {
-            appDetails.packagename = args.packagename;
-        }
-        if (args.name) {
-            appDetails.name = args.name;
-        }
-        if (args.version) {
-            appDetails.version = args.version;
-        }
-        if (args.options) {
-            appDetails.options = args.options.split(',');
-        }
-    } else {
-        appDetails = await infoprompt.askAppCredentials();
-    }
-
-    var haveKJar = false;
-    var haveDKJar = false;
-    if (appDetails.options.some(e => e === 'kjar')) {
-        haveKJar = true;
-    }
-    if (appDetails.options.some(e => e === 'dkjar')) {
-        haveDKJar = true;
-    }
-
-    if (haveKJar && haveDKJar) {
-        appDetails.options.shift();
-    }
-
-    appinstall.getAndGenerate(site, dounzip, appDetails);
-}
 
 const args = minimist(process.argv.slice(2))
 
@@ -91,7 +42,7 @@ if (args._.length != 1) {
     if (allCommands.indexOf(cmd) < 0) {
         console.log(usage.showUsage());
     } else {
-        genAndInstall(args);
+        gen.getAndGenerate(args, quickinstall, site, dounzip);
     }
 
 }
